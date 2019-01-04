c028e1994f53b8cadcd0e0fa5f6c084eb6c7d180
X <46359328+ChimericalCodex@users.noreply.github.com>
2019-01-04 00:00:52 -0800

    Add series description and tag tooltips.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 	"use strict";
 
 	var collapsibles = document.getElementsByClassName("collapsible");
-
 	for (var i = 0; i < collapsibles.length; i++) {
 		collapsibles[i].addEventListener("click", function() {
 			this.classList.toggle("active");
@@ -14,4 +13,27 @@
 			}
 		});
 	}
+
+	/**
+	 * Add a tooltip to all of a given class that will appear on hover.
+	 */
+	function addTooltip(className, tooltipHtml) {
+		var tags = document.getElementsByClassName(className);
+		for (var i = 0; i < tags.length; i++) {
+			var tooltip = document.createElement("span")
+			tooltip.classList.add("tooltip-text");
+			tooltip.innerHTML = tooltipHtml
+			tags[i].appendChild(tooltip);
+		}
+	}
+
+	addTooltip("difficulty-tag-easy", "Intended for players familiar with Minecraft survival mode.");
+	addTooltip("difficulty-tag-medium", "Well rounded challenge intended for players with minimal to moderate CTM experience.");
+	addTooltip("difficulty-tag-hard", "Intended for CTM veterans or anyone looking to test their mettle.");
+
+	addTooltip("length-tag-short", "Small, contained maps, usually with most gear being recieved at the start.");
+	addTooltip("length-tag-medium", "Moderately sized with some element of exploration. Gear is a good mix of being given and needing to be found.");
+	addTooltip("length-tag-long", "Comparable scale to an area in a full CTM map. Usually requires more collecting, crafting, and general survival.");
+
+	addTooltip("version-tag", "Compatible Minecraft versions");
 })();
